29a32071b425b88cd55636dbec960a94421ade6b
eoto0 <eoto0@chicagobooth.edu>
2025-04-10 00:59:04 +0000

    first iteration

--- umbrella.rb
+++ umbrella.rb
@@ -1,1 +1,53 @@
 # Write your soltuion here!
+
+require "http"
+require "json"
+require "dotenv/load"
+
+gmaps_api_key =  ENV.fetch("GMAPS_KEY")
+pirate_api_key = ENV.fetch("PIRATE_WEATHER_KEY")
+
+pp "Where are you located?"
+
+#user_location = gets.chomp
+
+user_location = "Chicago"
+
+gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + user_location + "&key=" + gmaps_api_key
+
+
+resp = HTTP.get(gmaps_url)
+
+raw_response = resp.to_s
+
+require "json"
+
+parsed_response = JSON.parse(raw_response)
+
+results = parsed_response.fetch("results")
+
+#pp results
+
+first_results = results.at(0)
+
+pp first_results.keys
+
+geometry = first_results.fetch("geometry") 
+
+pp geometry.keys
+
+location = geometry.fetch("location")
+
+
+lat = location.fetch("lat")
+lng = location.fetch("lng")
+
+pp location
+
+#pp gmaps_url
+
+#raw_response = HTTP.get(gmaps_url)
+
+#pp raw_response
+
+#parsed_response = JSON.parse(raw_response)
